3e03f676d9f1b3b5c8d47eff78d8ccd571adc29c
jaeheonYoo325 <yoojh0325@gmail.com>
2021-10-23 23:28:38 +0900

    2021.10.23 : Remember Me 인증.

--- src/main/java/io/security/basic/security/config/SecurityConfig.java
+++ src/main/java/io/security/basic/security/config/SecurityConfig.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -19,6 +21,9 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	UserDetailsService userDetailsService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -73,9 +78,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
 							throws IOException, ServletException {
 
+						System.out.println("logout");
 						response.sendRedirect("/login");
 					}
 				})
-				.deleteCookies("remember-me");
+				.deleteCookies("remember-me")
+		.and()
+				.rememberMe()
+				.rememberMeParameter("remember")
+				.tokenValiditySeconds(3600)
+				.userDetailsService(userDetailsService);
 	}
 }
