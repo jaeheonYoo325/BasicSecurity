dedc13c569da2e3356a2fc4e949417223ebbbd31
jaeheonYoo325 <yoojh0325@gmail.com>
2021-10-30 11:34:56 +0900

    2021.10.30 : 권한설정과 표현식.

--- src/main/java/io/security/basic/security/config/SecurityConfig.java
+++ src/main/java/io/security/basic/security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,13 +25,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	UserDetailsService userDetailsService;
 	
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+		auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS","USER");
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN","SYS","USER");
+	}
+
+
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests()
+				.antMatchers("/user").hasRole("USER")
+				.antMatchers("/admin/pay").hasRole("ADMIN")				
+				.antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
 				.anyRequest().authenticated();
 		http
-				.formLogin()
+				.formLogin();
 //				.loginPage("/loginPage")
 //				.defaultSuccessUrl("/")
 //				.failureUrl("/login")
@@ -89,10 +104,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //				.tokenValiditySeconds(3600)
 //				.userDetailsService(userDetailsService);
 		
-		.and()
-				.sessionManagement()
-				.maximumSessions(1)
-				.maxSessionsPreventsLogin(false);
+//		.and()
+//				.sessionManagement()
+//				.maximumSessions(1)
+//				.maxSessionsPreventsLogin(false);
+				
 //		http
 //				.sessionManagement()
 //				.sessionFixation().changeSessionId();
